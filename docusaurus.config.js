cee8dc4e19d539882d479ace868b1611d5d103b9
Georg <georg.strassberger@googlemail.com>
2025-06-12 19:50:07 +0200

    clear up navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,148 +1,126 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import {themes as prismThemes} from 'prism-react-renderer';
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'DevOps-Zone',
-  tagline: 'System Documentation',
-  favicon: 'img/favicon.ico',
-
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
-
-  // Set the production url of your site here
-  url: 'https://devops-zone.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'GEST', // Usually your GitHub org/user name.
-  projectName: 'SysDocs', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'de',
-    locales: ['de'],
-  },
-
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/GeorgStrassberger',
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/GeorgStrassberger',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'DevOps-Zone',
-        logo: {
-          alt: 'devops-zone-logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'iotSidebar',
-            position: 'left',
-            label: 'IoT',
-          },
-          // {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+    title: 'DevOps-Zone',
+    tagline: 'System Documentation',
+    favicon: 'img/favicon.ico',
+    future: {
+        v4: true,
+    },
+    url: 'https://devops-zone.com',
+    baseUrl: '/',
+    organizationName: 'GEST',
+    projectName: 'SysDocs',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    i18n: {
+        defaultLocale: 'de',
+        locales: ['de'],
+    },
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: './sidebars.js',
+                    editUrl:
+                        'https://github.com/GeorgStrassberger',
+                },
+                blog: {
+                    showReadingTime: true,
+                    feedOptions: {
+                        type: ['rss', 'atom'],
+                        xslt: true,
+                    },
+                    editUrl:
+                        'https://github.com/GeorgStrassberger',
+                    onInlineTags: 'warn',
+                    onInlineAuthors: 'warn',
+                    onUntruncatedBlogPosts: 'warn',
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            }),
         ],
-      },
-      footer: {
-        style: 'dark',
-        // links: [
-        //   {
-        //     title: 'Docs',
-        //     items: [
-        //       {
-        //         label: 'Tutorial',
-        //         to: '/docs/intro',
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     title: 'Community',
-        //     items: [
-        //       {
-        //         label: 'X',
-        //         href: 'https://x.com/Ge_St_86',
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     title: 'More',
-        //     items: [
-        //       {
-        //         label: 'Blog',
-        //         to: '/blog',
-        //       },
-        //       {
-        //         label: 'GitHub',
-        //         href: 'https://github.com/GeorgStrassberger',
-        //       },
-        //     ],
-        //   },
-        // ],
-        copyright: `Copyright © ${new Date().getFullYear()} Sysdocs.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+    ],
+
+    themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            // Replace with your project's social card
+            image: 'img/docusaurus-social-card.jpg',
+            navbar: {
+                title: 'DevOps-Zone',
+                logo: {
+                    alt: 'devops-zone-logo',
+                    src: 'img/logo.svg',
+                },
+                items: [
+                    {
+                        type: 'docSidebar',
+                        sidebarId: 'iotSidebar',
+                        position: 'left',
+                        label: 'IoT',
+                    },
+                    {
+                        type: 'docSidebar',
+                        sidebarId: 'linuxSidebar',
+                        position: 'left',
+                        label: 'Linux',
+                    },
+                    // {to: '/blog', label: 'Blog', position: 'left'},
+                    {
+                        href: 'https://github.com/facebook/docusaurus',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                // links: [
+                //   {
+                //     title: 'Docs',
+                //     items: [
+                //       {
+                //         label: 'Tutorial',
+                //         to: '/docs/intro',
+                //       },
+                //     ],
+                //   },
+                //   {
+                //     title: 'Community',
+                //     items: [
+                //       {
+                //         label: 'X',
+                //         href: 'https://x.com/Ge_St_86',
+                //       },
+                //     ],
+                //   },
+                //   {
+                //     title: 'More',
+                //     items: [
+                //       {
+                //         label: 'Blog',
+                //         to: '/blog',
+                //       },
+                //       {
+                //         label: 'GitHub',
+                //         href: 'https://github.com/GeorgStrassberger',
+                //       },
+                //     ],
+                //   },
+                // ],
+                copyright: `Copyright © ${new Date().getFullYear()} Sysdocs.`,
+            },
+            prism: {
+                theme: prismThemes.github,
+                darkTheme: prismThemes.dracula,
+            },
+        }),
 };
 
 export default config;
